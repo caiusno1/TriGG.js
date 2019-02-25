25bd5ed0ec3f46529048b452a2c407ab19c3e225
Kai Biermeier <kbiermeier1@web.de>
2019-02-25 21:30:25 +0100

    initialize project

---
+++ engine/core/TriGG-engine.js
@@ -0,0 +1,23 @@
+import {nools} from 'nools';
+var TGGRule = function(src,trg,corr){
+    this.src = src;
+    this.trg = trg;
+    this.corr = corr;
+}
+var triggEngine = function(){
+    this.flow = nools.flow("TGG-Flow", function (facts){
+        
+    })
+}
+triggEngine.prototype.load = function(rules){
+
+}
+triggEngine.prototype.addRule = function(rule){
+
+}
+triggEngine.prototype.deleteRule = function(rule){
+
+}
+triggEngine.prototype.forwardSync = function(srcDelta){
+    
+}
